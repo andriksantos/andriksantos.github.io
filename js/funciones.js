f24eaacff0a5d4f4b16290deaaec991d76def011
Andrick Santos <andricksantos1@gmail.com>
2018-01-03 12:46:28 -0600

    restructuracion de la pagina

---
+++ js/funciones.js
@@ -0,0 +1,4 @@
+var mostrarValor = function(){//funcion para cambiar el label en el formulario de contacto
+	var x = document.getElementById("mySelect").value;
+    document.getElementById("label").innerHTML =  x;
+	}
